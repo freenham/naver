934a8757dcd2e3faec63160598b234a5c06dcca1
freenham <freenham@gmail.com>
2017-06-19 22:55:50 +0900

    근무시간문제 수정

--- Naver/src/naver/N278915803.java
+++ Naver/src/naver/N278915803.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class N278915803 {
 	public static void main(String[] args) {
-		double base=4.10;
-		double total=0;
+		double base = 4.10;
+		double total = 0;
 		System.out.print("근무시간 입력 : ");
 		Scanner s = new Scanner(System.in);
 		int time = s.nextInt();
-		if(time>=40)
-			base *= 1.5;
-		total = time * base;
-		System.out.printf("총 임금은 %.1f만원 입니다.",total);
-		
-		
+		if (time >= 40)
+			total = ((time - 40) * base * 1.5) + 40 * base;
+		else 
+			total = time * base;
+		System.out.printf("총 임금은 %.1f만원 입니다.", total);
+
 	}
 }
