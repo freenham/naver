952282ba098d8ac667e616de9c2a0980544bbae0
JungHyun <freenham@gmail.com>
2017-07-07 14:39:44 +0900

    예외 발생

---
+++ Naver/src/naver/N280031025.java
@@ -0,0 +1,28 @@
+package naver;
+
+public class N280031025 {
+
+	public static int searchArray(int[] array, int a) {
+		
+
+		try {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == a)
+					return i;
+			}
+			throw new NotFoundExecption("배열에서 찾을 수 없습니다.");
+			
+		} catch (NotFoundExecption e) {
+			e.printStackTrace();
+		}
+		return -1;
+		
+	}
+
+	public static void main(String[] args) {
+
+		int[] a = new int[] { 1, 2, 3, 4, 5 };
+		int b = searchArray(a, 11);
+//		System.out.println(b);
+	}
+}
---
+++ Naver/src/naver/NotFoundExecption.java
@@ -0,0 +1,11 @@
+package naver;
+
+public class NotFoundExecption extends Exception {
+
+	public NotFoundExecption(String message) {
+
+		super(message);
+
+	}
+
+}
