9f62db6109b12364f7e10e0c7ede83aedcc0284b
JungHyun <freenham@gmail.com>
2017-07-10 18:51:23 +0900

    날짜 1000

---
+++ Naver/src/naver/N280232987.java
@@ -0,0 +1,10 @@
+package naver;
+
+import java.util.Date;
+
+public class N280232987 {
+	public static void main(String[] args) {
+		Date today = new Date();
+		System.out.println(today.getTime()/(3600*24*1000*365));
+	}
+}
